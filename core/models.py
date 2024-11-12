8876ef0a843340c87979d84e3f6dec8ca81005cf
Precieuse10 <agneroprecieuse@gmail.com>
2024-11-12 11:29:03 +0000

    finalisation de MVP

--- core/models.py
+++ core/models.py
@@ -16,3 +16,9 @@ class Predications(models.Model):
 
     def __str__(self):
         return f"{self.titre} - {self.orateur}"
+    
+class EgliseInfo(models.Model):
+    adresse = models.CharField(max_length=200)
+    email = models.EmailField()
+    telephone = models.CharField(max_length=20)
+    programme_hebdomadaire = models.TextField()
